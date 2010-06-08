797b4e1c30e51a15d58ff8c4ddda19269ff825b9
Lubosz <lubosz@gmail.com>
2010-06-08 07:11:29 +0200

    Mesh factory added

---
+++ include/MeshFactory.h
@@ -0,0 +1,34 @@
+/*
+ * MeshFactory.h
+ *
+ *  Created on: Jun 8, 2010
+ *      Author: bmonkey
+ */
+
+#pragma once
+
+#include "Mesh.h"
+
+class MeshFactory {
+public:
+
+	static MeshFactory& Instance()
+	 {
+	    static MeshFactory singleton;
+	    return singleton;
+	  }
+
+	void drawMeshes();
+	void tetrahedron();
+	void addMesh();
+	void loadObj(string file);
+
+	vector<Mesh*> meshes;
+
+private:
+	MeshFactory();                                 // Private constructor
+	~MeshFactory();
+	MeshFactory(const MeshFactory&);                 // Prevent copy-construction
+	MeshFactory& operator=(const MeshFactory&);      // Prevent assignment
+};
+
