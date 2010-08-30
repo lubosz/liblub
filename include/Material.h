81d634d7c99c7430fe827fdff894fac9e6f5fc7a
Lubosz <lubosz@gmail.com>
2010-08-30 19:15:31 +0200

    Material code extracted to cpp

--- include/Material.h
+++ include/Material.h
@@ -19,104 +19,34 @@ using namespace std;
 
 class Material {
 public:
+    ShaderProgram *getShaderProgram() const;
+//private:
 	ShaderProgram * shaderProgram;
 	vector<Texture*> textures;
 
 	virtual void uniforms() = 0;
 
-	void init(){
-		shaderProgram = new ShaderProgram();
-	}
+	void init();
 
-	void addTexture(Texture * texture){
-		textures.push_back(texture);
+	void addTexture(Texture * texture);
+	void addTexture(string file,string name);
+	void addTextureCube(string file,string name);
 
-	}
+	void defaultAttribs();
 
-	void addTexture(string file,string name){
-		textures.push_back(TextureFactory::Instance().load(file,name));
+	void attachVertFrag(string file);
+	void attachVertGeom(string file);
 
-	}
-	void addTextureCube(string file,string name){
-		textures.push_back(TextureFactory::Instance().loadCubeMap(file,name));
+	void attachVertFragGeom(string file);
+	void done();
 
-	}
+	void initUniforms();
+	void activateTextures();
 
-	void defaultAttribs(){
-
-		shaderProgram->bindAttrib("in_Vertex");
-		//TODO: Buffer order not variable
-		//shaderProgram->bindAttrib("in_Color");
-		shaderProgram->bindAttrib("in_Normal");
-		shaderProgram->bindAttrib("in_Uv");
-
-	}
-
-	void attachVertFrag(string file){
-		shaderProgram->attachShader(file + ".vert", GL_VERTEX_SHADER);
-		shaderProgram->attachShader(file + ".frag", GL_FRAGMENT_SHADER);
-	}
-
-	void attachVertGeom(string file){
-		shaderProgram->attachShader(file + ".vert", GL_VERTEX_SHADER);
-		shaderProgram->attachShader(file + ".geom", GL_GEOMETRY_SHADER);
-	}
-
-	void attachVertFragGeom(string file){
-		shaderProgram->attachShader(file + ".vert", GL_VERTEX_SHADER);
-		shaderProgram->attachShader(file + ".geom", GL_GEOMETRY_SHADER);
-		shaderProgram->attachShader(file + ".frag", GL_FRAGMENT_SHADER);
-	}
-
-	void done(){
-		defaultAttribs();
-		shaderProgram->linkAndUse();
-
-		initUniforms();
-		glError("Material",68);
-	}
-
-	void initUniforms(){
-		cout << "\nInitializing Material Uniforms for Shader #"<<shaderProgram->program<<"\n";
-		/*
-		GLint texLoc;
-		for (int i = 0; i < textures.size(); i++){
-			cout << "Assigning Texture " << i << " " << textures[i]->name << "\n";
-			textures[i]->activate();
-			textures[i]->bind();
-			texLoc   = glGetUniformLocation(shaderProgram->program, textures[i]->name.c_str());
-			glUniform1i(texLoc, i);
-		}
-		*/
-		glError("Material",84);
-        BOOST_FOREACH( Texture* texture, textures ){
-        	texture->activate();
-        	texture->bind();
-        	texture->uniform(shaderProgram->program);
-        }
-
-		uniforms();
-		glError("Material",90);
-
-	}
-
-	void activateTextures(){
-	    BOOST_FOREACH( Texture* texture, textures )
-	    {
-	    	texture->activate();
-	    	texture->bind();
-
-	    }
-		glError("Material",102);
-	}
-
-	void activate(){
-
-        //shaderProgram->use();
-        activateTextures();
-		glError("Material::activate",109);
-
-	}
+	void activate();
 };
+
+
+
 
 
