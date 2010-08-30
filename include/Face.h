03e65c35e7d35ddcf3f7cc926cda0e1603818d49
Lubosz <lubosz@gmail.com>
2010-08-30 19:09:09 +0200

    Made stuff in Geometry class private

--- include/Face.h
+++ include/Face.h
@@ -9,12 +9,10 @@
 
 class Face {
 public:
-
-	unsigned vertIndices[3], normalIndices[3], uvIndices[3];
-
 	Face(unsigned vertex1, unsigned vertex2, unsigned vertex3);
+	virtual ~Face();
 	void setNormalIndices(unsigned normal1, unsigned normal2, unsigned normal3);
 	void setUvIndices(unsigned uv1, unsigned uv2, unsigned uv3);
-	virtual ~Face();
+	unsigned vertIndices[3], normalIndices[3], uvIndices[3];
 };
 
