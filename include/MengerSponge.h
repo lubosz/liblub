a1837991645551e7f48d501b1dae67f9522e827b
Lubosz <lubosz@gmail.com>
2010-08-31 02:39:08 +0200

    MengerSponge single draw call WIP

--- include/MengerSponge.h
+++ include/MengerSponge.h
@@ -16,8 +16,13 @@ private:
 	Mesh * mesh;
 	Material * material;
 
-	void makeSponge(unsigned recursion,vector<float> position, float size);
+	vector<GLfloat> oneMeshVertices;
+	vector<GLfloat> oneMeshUVCoords;
+	vector<GLuint> oneMeshIndices;
+
+	void makeSponge(unsigned recursion,const vector<float> & position, float size);
 	void makeCube();
+	void addCube(const vector<float> & position, float size);
 
 public:
 	MengerSponge(unsigned recursion);
