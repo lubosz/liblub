04e4e32dde832eb8c9a4c83c7252ccbe6f9ba9be
Lubosz <lubosz@gmail.com>
2010-10-02 04:50:43 +0200

    deleted old files

--- examples/nice.cpp
+++ examples/nice.cpp
@@ -17,19 +17,26 @@ public:
 
 		vector<Material*> textureMats = {
 
-				new TextureMaterial("bunny.png"),
-				new MultiTextureMaterial(),
+				//new TextureMaterial("bunny.png"),
+				//new MultiTextureMaterial(),
 				//new EarthMaterial(),
 				new EnvMat(),
 				new NormalColorMat(),
-				new BrickMaterial(),
-				new ProcBumpMaterial(),
+				//new BrickMaterial(),
+				//new ProcBumpMaterial(),
 				new OceanMat(),
+				new NormalMapMaterial("bump/brickwork-texture.jpg","bump/brickwork_normal-map.jpg"),
+				new PhongColorMat(QVector3D(0,1,0)),
+				new SphereMap("spectrum.png", "spheremaps/spheremap_bar.jpg"),
+
+				new PhongTexMat("Paper_Texture_by_Spiteful_Pie_Stock.jpg"),
 
 				new MandelMat()
 		};
-		SceneGraph::Instance().meshPlane("monkeySmooth.obj", 10, 3, textureMats);
-		SceneGraph::Instance().addNode(new Node("",{0,-50,-50},MeshFactory::Instance().stars(),new StarMat()));
+		SceneGraph::Instance().meshPlane("monkey.blend", 10, 3, textureMats);
+		Material * starMat = new StarMat();
+		SceneGraph::Instance().addNode(new Node("",{0,-50,-50},MeshFactory::Instance().stars(),starMat));
+		//SceneGraph::Instance().addNode(new Node("Spiral",{0,0,0}, MeshFactory::Instance().spiral(),starMat));
 	}
 };
 
