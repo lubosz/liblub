314aa073b2356701c2fe21831a1c621916f1dfb3
Lubosz <lubosz@gmail.com>
2010-11-21 22:44:30 +0100

    cleaned up xcb code

--- src/Window/XCB/MediaLayer.cpp
+++ src/Window/XCB/MediaLayer.cpp
@@ -83,42 +83,6 @@ void MediaLayer::initScreen(){
 
 	width = screen->width_in_pixels;
 	height = screen->height_in_pixels;
-/*
-    printf("white_pixel: %d\n \
-			black_pixel: %d\n \
-			current_input_masks: %d\n \
-			width_in_pixels: %d\n \
-			height_in_pixels: %d\n \
-			width_in_millimeters: %d\n \
-			height_in_millimeters: %d\n \
-			min_installed_maps: %d\n \
-			max_installed_maps: %d\n \
-			backing_stores: %d\n \
-			save_unders: %d\n \
-			root_depth: %d\n \
-			allowed_depths_len: %d\n",
-
-			screen->white_pixel,
-			screen->black_pixel,
-			screen->current_input_masks,
-			screen->width_in_pixels,
-			screen->height_in_pixels,
-			screen->width_in_millimeters,
-			screen->height_in_millimeters,
-			screen->min_installed_maps,
-			screen->max_installed_maps,
-			screen->backing_stores,
-			screen->save_unders,
-			screen->root_depth,
-			screen->allowed_depths_len
-
-    );
-    */
-    //xcb_window_t   root;
-    //xcb_colormap_t default_colormap;
-    //xcb_visualid_t root_visual;
-
-
 
 }
 
@@ -137,9 +101,6 @@ void MediaLayer::createGLContext() {
 
 	/* Select first framebuffer config and query visualID */
 	glXGetFBConfigAttrib(display, fb_config, GLX_VISUAL_ID, &visualID);
-
-	/* Simple OpenGL context */
-	//context = glXCreateNewContext(display, fb_config, GLX_RGBA_TYPE, 0, True);
 
 	int attribs[] = {
 			GLX_CONTEXT_MAJOR_VERSION_ARB,
@@ -171,17 +132,9 @@ void MediaLayer::createColorMap() {
 
 void MediaLayer::createWindow() {
 	eventmask =
-			//XCB_EVENT_MASK_EXPOSURE |
 			XCB_EVENT_MASK_KEY_PRESS |
 			XCB_EVENT_MASK_POINTER_MOTION |
 			XCB_EVENT_MASK_KEY_RELEASE
-			/*
-			|
-	 		XCB_EVENT_MASK_BUTTON_PRESS |
-			XCB_EVENT_MASK_BUTTON_RELEASE |
-			XCB_EVENT_MASK_ENTER_WINDOW |
-			XCB_EVENT_MASK_LEAVE_WINDOW
-			 */
 			;
 	uint32_t valuelist[] = { eventmask, colormap };
 	uint32_t valuemask = XCB_CW_EVENT_MASK | XCB_CW_COLORMAP;
@@ -202,28 +155,6 @@ void MediaLayer::createWindow() {
 
 	setWindowTitle(programTile);
 
-/*
-    static const uint32_t wm_hints[8] = {
-        3, // flags: Input, Initial state
-        1, // input: True
-        1, // initial state: Normal
-        0, 0, 0, 0, 0, // Icon
-    };
-
-    xcb_change_property (
-    		connection,
-    		XCB_PROP_MODE_REPLACE,
-    		window,
-    		XCB_ATOM_WM_HINTS,
-    		XCB_ATOM_WM_HINTS,
-            32,
-            8,
-            wm_hints
-    );
-*/
-
-
-
 	// NOTE: window must be mapped before glXMakeContextCurrent
 	xcb_map_window(connection, window);
 
@@ -241,19 +172,6 @@ void MediaLayer::createWindow() {
 
 	//Set swap interval
 	glXSwapIntervalSGI(VSync);
-
-	const static uint32_t values[] = { XCB_STACK_MODE_ABOVE };
-
-
-
-	/* Move the window on the top of the stack */
-	xcb_configure_window(
-			connection,
-			window,
-			XCB_CONFIG_WINDOW_STACK_MODE,
-			values
-	);
-
 }
 
 /* A simple function that prints a message, the error code returned by SDL, and quits the application */
@@ -275,13 +193,9 @@ void MediaLayer::toggleFullScreen() {
 		cout << "Fullscreen on\n";
 		fullscreen = true;
 	}
-
+/*
 	string wm_state_name = "_NET_WM_STATE";
 	string wm_state_fs_name = "_NET_WM_STATE_FULLSCREEN";
-	//string wm_state_fs_name = "_NET_WM_STATE_MAXIMIZED_VERT";
-
-    //atoms[count++] = data->_NET_WM_STATE_MAXIMIZED_VERT;
-    //atoms[count++] = data->_NET_WM_STATE_MAXIMIZED_HORZ;
 
 	xcb_intern_atom_cookie_t wm_state_ck = xcb_intern_atom(connection, 0, wm_state_name.length(), wm_state_name.c_str());
 	xcb_intern_atom_cookie_t wm_state_fs_ck = xcb_intern_atom(connection, 0, wm_state_fs_name.length(), wm_state_fs_name.c_str());
@@ -293,7 +207,6 @@ void MediaLayer::toggleFullScreen() {
 		fprintf(stderr, "Can't set the screen. Error Code: %i\n",error1->error_code);
 	wm_state = reply->atom;
 	free(reply);
-	//cout << error << "\n";
 
 	reply = xcb_intern_atom_reply(connection, wm_state_fs_ck, &error2);
 	if (error2)
@@ -303,20 +216,6 @@ void MediaLayer::toggleFullScreen() {
 
 	// From EWMH "_WM_STATE"
 	xcb_client_message_event_t ev;
-	/*
-	ev.response_type = XCB_CLIENT_MESSAGE;
-	ev.format = 32;
-	ev.window = window;
-	ev.type = wm_state;
-
-	//ev.data.data32[0] = fullscreen ? 1 : 0;
-	ev.data.data32[0] = 3; //toggle
-	ev.data.data32[1] = wm_state_fullscreen;
-	ev.data.data32[2] = 0;
-	ev.data.data32[3] = 1;
-	ev.data.data32[4] = 0;
-*/
-
 	ev.response_type = XCB_CLIENT_MESSAGE;
 	ev.type = wm_state;
 	ev.format = 32;
@@ -335,78 +234,13 @@ void MediaLayer::toggleFullScreen() {
 			XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY | XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT,
 			(const char *) &ev
 	);
-
-	/*
-	uint32_t borderWidth = 150;
-	xcb_configure_window(connection, window, XCB_CONFIG_WINDOW_BORDER_WIDTH, &borderWidth);
-	uint32_t newHeight = 150;
-	xcb_configure_window(connection, window, XCB_CONFIG_WINDOW_HEIGHT, &newHeight);
-	cout << "New height "<< newHeight << "\n";
-
-
-	xcb_configure_window(
-			connection,
-			window,
-			XCB_CONFIG_WINDOW_BORDER_WIDTH|XCB_CONFIG_WINDOW_STACK_MODE,
-			(uint32_t[]){0,XCB_STACK_MODE_BELOW}
-	);
 */
-
-
-
-/*
-	case XCB_CLIENT_MESSAGE:
-	{
-		xcb_client_message_event_t* clientEvent =
-				(xcb_client_message_event_t*) event;
-		if (clientEvent->type == displayData->WM_PROTOCOLS_ATOM) {
-			if (clientEvent->data.data32[0]
-					== displayData->WM_DELETE_WINDOW_ATOM) {
-				handleCloseWindowRequestEvent(env, getTopLevelWindowHandle(env,
-						javaDisplay, clientEvent->window));
-			} else if (clientEvent->data.data32[0]
-					== displayData->NET_WM_PING_ATOM) {
-				printf("Got ping\n");
-				clientEvent->response_type = XCB_CLIENT_MESSAGE;
-				clientEvent->window = screen->root;
-				xcb_send_event(connection, false, screen->root,
-						XCB_EVENT_MASK_STRUCTURE_NOTIFY
-								| XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT,
-						(const char*) &clientEvent);
-			}
-		}
-
-
-
-	xcb_client_message_event_t pingResponse;
-	pingResponse.response_type = XCB_CLIENT_MESSAGE;
-	pingResponse.format = 32;
-	pingResponse.sequence = clientEvent->sequence;
-	pingResponse.window = screen->root;
-	pingResponse.type = displayData->WM_PROTOCOLS_ATOM;
-
-	for (int i = 0; i < 5; i++) {
-		pingResponse.data.data32[i] = clientEvent->data.data32[i];
-	}
-
-	xcb_send_event(
-			connection,
-			false,
-			screen->root,
-			XCB_EVENT_MASK_STRUCTURE_NOTIFY | XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT,
-			(const char*) &pingResponse
-	);
-*/
-
-
-
 }
 
 void MediaLayer::renderLoop(){
     while (!quit) {
     	input->eventLoop();
     	RenderEngine::Instance().display();
-
     	swapBuffers();
     	getFPS();
      }
@@ -425,36 +259,23 @@ void MediaLayer::getFPS(){
            fps_current = fps_frames;
            fps_frames = 0;
     }
-    //windowTitle.clear();
+
     stringstream windowTitle;
     windowTitle << programTile << " - FPS: " << fps_current;
 	setWindowTitle(windowTitle.str());
 }
 
 void MediaLayer::setWindowTitle(string title){
-    xcb_change_property (connection,
-                         XCB_PROP_MODE_REPLACE,
-                         window,
-                         XCB_ATOM_WM_NAME,
-                         XCB_ATOM_STRING,
-                         8,
-                         title.length(),
-                         title.c_str()
-    );
-
-/* set the title of the window icon */
-/*
-    char *iconTitle = "Hello World ! (iconified)";
-    xcb_change_property (connection,
-                         XCB_PROP_MODE_REPLACE,
-                         windows,
-                         WM_ICON_NAME,
-                         STRING,
-                         8,
-                         strlen(iconTitle),
-                         iconTitle);
-*/
-
+    xcb_change_property(
+    	connection,
+    	XCB_PROP_MODE_REPLACE,
+    	window,
+		XCB_ATOM_WM_NAME,
+		XCB_ATOM_STRING,
+		8,
+		title.length(),
+		title.c_str()
+	);
 }
 
 void MediaLayer::toggleMouseGrab(){
@@ -468,10 +289,6 @@ void MediaLayer::toggleMouseGrab(){
                 XCB_CW_CURSOR,
                 &value_list
         );
-		/*
-		XGrabPointer(display, window, True, ButtonPressMask,
-			GrabModeAsync, GrabModeAsync, window, None, CurrentTime);
-			*/
 		grab = true;
 	}else{
 	    /* show the default cursor */
@@ -482,7 +299,6 @@ void MediaLayer::toggleMouseGrab(){
 	    		XCB_CW_CURSOR,
 	    		&value_list
 	    );
-		//XUngrabPointer(display, CurrentTime);
 		grab = false;
 	}
 }
@@ -498,6 +314,4 @@ void MediaLayer::mouseLook(int x, int y){
 		Camera::Instance().setMouseLook(xRel, yRel);
 		if (grab) XWarpPointer(display, None, window, x, y, width, height, halfWidth, halfHeight);
 	}
-
-    //cout << width << " " << height << " " << halfWidth << " " << halfHeight << " " << xRel << " " << yRel << "\n";
 }
