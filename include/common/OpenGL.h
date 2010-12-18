dd5da8e1804374398c2563bab40d33e56ba48834
Lubosz <lubosz@gmail.com>
2010-12-18 17:16:53 +0100

    set to opengl 4

--- include/common/OpenGL.h
+++ include/common/OpenGL.h
@@ -11,8 +11,8 @@
 //#ifdef USE_GL3
 #define GL3_PROTOTYPES 1
 //
-#define LIBLUB_GL_MAJOR_VERION 3
-#define LIBLUB_GL_MINOR_VERION 3
+#define LIBLUB_GL_MAJOR_VERION 4
+#define LIBLUB_GL_MINOR_VERION 1
 //#ifndef GLfloat
 #include <GL3/gl3.h>
 //Hack for GL2 Extensions in GL3
