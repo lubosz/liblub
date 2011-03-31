871430ff42ec7c96dcd68c8db8f5ce1650858e4f
Lubosz <lubosz@gmail.com>
2011-03-31 17:45:24 +0200

    places where to put glew (just in case), but it segfaults

--- include/common/OpenGL.h
+++ include/common/OpenGL.h
@@ -8,5 +8,10 @@
 #pragma once
 
 #define GL3_PROTOTYPES 1
+//#include <GL/glew.h>
 #include <GL3/gl3.h>
 #include <GL3/glext.h>
+
+//copied from glew
+//#define GL_DEPTH_SCALE 0x0D1E
+//#define GL_DEPTH_BIAS 0x0D1F
