a42704722798e1ed1b78f91efe84cfd6def11771
Lubosz Sarnecki <lubosz@gmail.com>
2011-05-06 04:50:11 +0200

    adjusted tessterrain size

--- src/Planets/TerrainTesselation.cpp
+++ src/Planets/TerrainTesselation.cpp
@@ -53,7 +53,7 @@ void TerrainTesselation::init(const QVector3D& position, float size){
    Mesh * groundMesh = MeshLoader::load(attributes, "earth.obj");
 //    Mesh * mesh = Geometry::gluSphere(10.0f, 100, 50);
    groundMesh->setDrawType(GL_PATCHES);
-   terrainNode = new Node("ground", position, size*12.1, groundMesh, terrainMat);
+   terrainNode = new Node("ground", position, size*11.5, groundMesh, terrainMat);
 
    GUI::Instance().addText("tess", "Tess");
    GUI::Instance().addText("dist", "Dist");
