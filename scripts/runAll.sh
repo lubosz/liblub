8a9953c958db5a62f58b66b073965a896c2abc24
Lubosz Sarnecki <lubosz@gmail.com>
2011-08-19 06:07:32 +0200

    run all xml files in runAll.sh

--- scripts/runAll.sh
+++ scripts/runAll.sh
@@ -9,7 +9,17 @@ function error {
 EXAMPLES=`ls bin`
 
 for EXAMPLE in $EXAMPLES; do
+echo -n "Press Enter to start $EXAMPLE"
+read answer
   bin/$EXAMPLE || error $EXAMPLE
+done
+
+SCENES=`ls media/scenes/ | grep "xml" | sed 's/\(.*\)\..*/\1/'`
+
+for SCENE in $SCENES; do
+echo -n "Press Enter to start $SCENE.xml"
+read answer
+  bin/load $SCENE || error $SCENE
 done
 
 
