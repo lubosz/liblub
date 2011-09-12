491e50c01263d2d128003c346164646ff4242d2c
Lubosz Sarnecki <lubosz@gmail.com>
2011-09-12 05:27:58 +0200

    mesh : check for needed attributes in mesh loader

--- src/Mesh/MeshLoader.cpp
+++ src/Mesh/MeshLoader.cpp
@@ -17,22 +17,18 @@
 #include "Renderer/RenderEngine.h"
 
 Mesh * MeshLoader::load(const QList<string> & attributes, string file) {
+    LogDebug << "Loading" << file;
     Mesh * mesh = new Mesh(attributes);
     string path = Config::Instance().value<string> ("meshDir") + file;
 
-    // Create an instance of the Importer class
     Assimp::Importer importer;
 
-    // And have it read the given file with some example postprocessing
-    // Usually - if speed is not the most important aspect for you - you'll
-    // propably to request more postprocessing than we do in this example.
     const aiScene* scene = importer.ReadFile(path,
             aiProcess_CalcTangentSpace
             | aiProcess_Triangulate);
             // | aiProcess_JoinIdenticalVertices
             // | aiProcess_SortByPType
 
-    // If the import failed, report it
     if (!scene) {
         LogError << "Assimp Scene Load"<< importer.GetErrorString();
     }
@@ -68,26 +64,27 @@ Mesh * MeshLoader::load(const QList<string> & attributes, string file) {
                 boundingBoxMax.setZ(position.z);
             }
 
-            if(assMesh->HasNormals()) {
+            if(assMesh->HasNormals() && attributes.contains("normal")) {
               aiVector3D normal = assMesh->mNormals[vertex];
               mesh->vertex("normal",normal.x, normal.y, normal.z);
             }
 
-            if (assMesh->HasTangentsAndBitangents()) {
+            if (assMesh->HasTangentsAndBitangents() && attributes.contains("tangent")) {
               aiVector3D tangent = assMesh->mTangents[vertex];
               mesh->vertex("tangent",tangent.x, tangent.y, tangent.z);
+            }
 
+            if (assMesh->HasTangentsAndBitangents() && attributes.contains("bitangent")) {
               aiVector3D bitangent = assMesh->mBitangents[vertex];
               mesh->vertex("bitangent",bitangent.x, bitangent.y, bitangent.z);
             }
 
-            if(assMesh->HasTextureCoords(0)){
+            if(assMesh->HasTextureCoords(0) && attributes.contains("uv")){
               aiVector3D uv = assMesh->mTextureCoords[0][vertex];
               mesh->vertex("uv",uv.x, uv.y);
             }
         }
     }
-
 
     mesh->init();
     mesh->setDrawType(GL_TRIANGLES);
