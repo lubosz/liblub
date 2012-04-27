5a3c59ff8c27bbf518eab6cb8c2f3f0f72172245
Lubosz Sarnecki <lubosz@gmail.com>
2012-04-27 13:33:18 +0200

    new test

---
+++ tests/vertarrays.cpp
@@ -0,0 +1,36 @@
+#include <cppunit/TestCase.h>
+
+#include <QRectF>
+#include "Mesh/Geometry.h"
+#include "Mesh/MeshLoader.h"
+#include "System/Config.h"
+#include "Mesh/MengerSponge.h"
+#include "Window/XCB/XCBWindow.h"
+
+class MaxArraysTest: public CppUnit::TestCase {
+public:
+  MaxArraysTest(std::string name) :
+    CppUnit::TestCase(name) {
+  }
+
+  void runTest() {
+      Config::Instance().load("config.xml");
+      XCBWindow * window  = new XCBWindow();
+
+      for(int i = 0; i < 50; i++) {
+            GLuint vao;
+            glGenVertexArrays(1, &vao);
+            LogDebug << "Generating Vertex Array Object #" << vao;
+            glError;
+      }
+  }
+};
+
+int main() {
+  MaxArraysTest foo2("bar");
+  foo2.runTest();
+}
+
+
+
+
