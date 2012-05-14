aeb99c859c125242552ae2b1f5380867b3acb3b4
Lubosz Sarnecki <lubosz@gmail.com>
2012-05-14 15:55:29 +0200

    update valgrind scripts

--- scripts/valgrindStats.sh
+++ scripts/valgrindStats.sh
@@ -1,9 +1,11 @@
+   #--show-reachable=yes \
 valgrind \
    --leak-check=full \
-   --show-reachable=yes \
    --error-limit=no \
    --track-origins=yes \
    --suppressions=scripts/valgrind/external.supp \
+   --suppressions=scripts/valgrind/internal.supp \
+   --suppressions=scripts/valgrind/gui.supp \
    --suppressions=scripts/valgrind/toFix.supp \
    -v \
-   ./test/mesh
+   $1
--- scripts/writeSuppressions.sh
+++ scripts/writeSuppressions.sh
@@ -1,11 +1,17 @@
 valgrind \
    --leak-check=full \
-   --show-reachable=yes \
+   \ #--show-reachable=yes \
    --error-limit=no \
    --gen-suppressions=all \
    --track-origins=yes \
    --log-file=minimalraw.log \
    --suppressions=scripts/valgrind/external.supp \
-   ./test/mesh
+   --suppressions=scripts/valgrind/internal.supp \
+   --suppressions=scripts/valgrind/gui.supp \
+   $1
 
 cat ./minimalraw.log | Parse_valgrind_suppressions.sh > scripts/valgrind/current.supp
+
+./scripts/makeToFixList.py scripts/valgrind/current.supp > scripts/valgrind/toFix.supp
+
+./scripts/makeLibSuppressions.py > scripts/valgrind/libs.supp
