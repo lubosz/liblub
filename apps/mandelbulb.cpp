7447a65d43980fc9f00f0f49776a7e2b53f29e78
Lubosz Sarnecki <lubosz@gmail.com>
2012-07-12 14:05:37 +0200

    A mandelbulbish point cloud

--- apps/mandelbulb.cpp
+++ apps/mandelbulb.cpp
@@ -27,52 +27,139 @@ class MandelbulbApp: public Application {
 public:
 
 
-  MandelbulbApp(int argc, char *argv[]) :
-    Application(argc, argv) {
-  }
+    MandelbulbApp(int argc, char *argv[]) :
+        Application(argc, argv) {
+    }
 
-  ~MandelbulbApp() {
-  }
+    ~MandelbulbApp() {
+    }
 
-  Mesh * bulb(const QList<string> & attributes, unsigned resolution, float density) {
-      Mesh * mesh = new Mesh(attributes);
+    double norm(QVector3D in) {
+        return QVector3D::dotProduct(in, in);
+    }
 
-      for(unsigned x = 0; x < resolution; x++) {
-          for(unsigned y = 0; y < resolution; y++) {
-              for(unsigned z = 0; z < resolution; z++) {
 
-              QVector3D position = QVector3D(x * density, y * density, z * density);
+    QVector3D TriplexPow(QVector3D vec, unsigned n) {
+        double z = vec.z();
+        double r = sqrt(vec.x()*vec.x() + vec.y() * vec.y() + vec.z() * vec.z());
+        double theta = 0;
+        if (vec.x() != 0)
+            theta = n * atan(vec.y()/vec.x());
+        double phi = 0;
+        if (r != 0)
+            phi = n * asin(z/r);
 
-              mesh->vertex("position", position);
-              double color = double(x)/double(resolution);
-              mesh->vertex("color", QVector3D(color, color, color));
-              }
-          }
-      }
+/*
+      LogDebug << "z" << z
+               << "r" << r
+               << "theta" << theta
+               << "phi" << phi;
+*/
+        return QVector3D(cos(theta)*cos(phi),
+                         sin(theta)*sin(phi),
+                         -sin(phi));
+    }
+    int Mandelbulb(QVector3D c) {
 
-      mesh->makeLinearIndex();
+        QVector3D p = QVector3D(0, 0, 0);
+        int i = 0;
+        while(i < 24 && norm(p) < 4) {
+            p = TriplexPow(p, 8) + c;
+            i++;
+            /*
+          LogDebug << "i" << i
+                   << "p" << p.x() << p.y() << p.z()
+                   << "norm(p)" << norm(p);
+                   */
+        }
+        return i;
+    }
 
-      mesh->init();
-      mesh->setDrawType(GL_POINTS);
-      return mesh;
-  }
 
-  void scene() {
-      QList<string> attributes = QList<string>() << "color";
-      Mesh* mandelBulb = bulb(attributes, 100, 0.1);
+    Mesh * bulb(const QList<string> & attributes, unsigned resolution, float density) {
+        Mesh * mesh = new Mesh(attributes);
+        double n = 100;
 
-      ShaderProgram * stars = new SimpleProgram("Particle/stars",attributes);
+//        double from = -1.1;
+//        double to = 1.1;
+//        double step = 2.2/n;
 
-      Node * plane = new Node("Plane", QVector3D(-5,-10,-15), 1, mandelBulb, stars);
-      SceneGraph::Instance().addNode(plane);
+        double from = -10;
+        double to = 10;
+        double step = 0.3;
 
-  }
-  void renderFrame() {
-      OnePass::draw();
-  }
+        QMap<double, unsigned> colors;
+
+        QVector3D position;
+
+        for(double x = from; x < to; x+=step) {
+
+            double progress = 0;
+            if (x < 0) {
+                progress = fabs(from) - fabs(x);
+            } else if (x >= 0) {
+                progress = fabs(from) + x;
+            }
+
+            LogInfo << (x - from) * 100 / (fabs(from) + fabs(to)) << "%";
+
+            for(double y = from; y < to; y+=step) {
+                for(double z = from; z < to; z+=step) {
+
+                    double z2 = 1.1;
+                    //                  double x = 0;
+                    //                  double y = 0;
+
+                    position = QVector3D(x * density, y * density, z * density);
+                    while(z2 >= -0.1 && Mandelbulb(position) < 24) {
+                        z2 -= 2.2/n;
+                        //                      LogDebug << z2;
+                    }
+
+                    double color = fabs(z2);
+
+                    if (color > 1) {
+                        mesh->vertex("position", position*10);
+                        mesh->vertex("color", QVector3D(color, color, color));
+                    }
+//                    colors[color]++;
+
+                    /*
+                    if (color != 0.11)
+                        LogDebug << color;
+                        */
+                }
+            }
+        }
+
+        /*
+        foreach (double col, colors.keys())
+            LogDebug << col << colors[col];
+        */
+
+        mesh->makeLinearIndex();
+
+        mesh->init();
+        mesh->setDrawType(GL_POINTS);
+        return mesh;
+    }
+
+    void scene() {
+        QList<string> attributes = QList<string>() << "color";
+        Mesh* mandelBulb = bulb(attributes, 100, 0.1);
+
+        ShaderProgram * stars = new SimpleProgram("Particle/stars",attributes);
+
+        Node * plane = new Node("Plane", QVector3D(-5,-10,-15), 1, mandelBulb, stars);
+        SceneGraph::Instance().addNode(plane);
+
+    }
+    void renderFrame() {
+        OnePass::draw();
+    }
 };
 
 int main(int argc, char *argv[]) {
-  MandelbulbApp(argc, argv).run();
+    MandelbulbApp(argc, argv).run();
 }
 
